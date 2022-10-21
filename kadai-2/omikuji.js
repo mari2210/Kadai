504c603209ea74b4ef546f8c1e960d43f35c0ac5
mariHirofuji <marihiro670@gmail.com>
2022-10-21 17:09:28 +0900

    [Finish] Webアプリケーションをつくってみよう-2

--- kadai-2/omikuji.js
+++ kadai-2/omikuji.js
@@ -1,10 +1,13 @@
 let username;
 let userresult;
-let name;
 
 username=prompt("お名前を教えてください");
+if (username==""){username="名無し";}
 document.getElementById("name").innerHTML = username;
-let rand = Math.floor( Math.random() * 5);
+
+
+
+let rand = Math.floor(Math.random()*5);
 if (rand == 0) {
   userresult = "大吉";
 }
